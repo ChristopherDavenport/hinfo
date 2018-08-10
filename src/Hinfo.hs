b392b12a6a8386f0f76aad16bcf5da083b522082
Christopher Davenport <Chris@ChristopherDavenport.tech>
2018-08-10 19:58:06 -0400

    Additional Checks on Hpack Model

--- src/Hinfo.hs
+++ src/Hinfo.hs
@@ -1,3 +1,4 @@
+{-# language OverloadedStrings #-}
 module Hinfo 
   (
     app
@@ -19,6 +20,14 @@ app = do
 
 
 execute :: AppOption -> PackageFile -> IO ()
-execute  Name        pf = T.putStrLn $ packageName pf
-execute  Version     pf = T.putStrLn $ packageVersion pf
-execute  Description pf = maybe (die "Missing Description") T.putStrLn $ packageDescription pf
+execute Name              pf = T.putStrLn $ packageName pf
+execute Version           pf = T.putStrLn $ packageVersion pf
+execute Github            pf = maybe (die "Missing Github") T.putStrLn (packageGithub pf)
+execute Author            pf = maybe (die "Missing Author") T.putStrLn (packageAuthor pf)
+execute Maintainer        pf = maybe (die "Missing Maintainer") T.putStrLn (packageMaintainer pf)
+execute Copyright         pf = maybe (die "Mising Copyright") T.putStrLn (packageCopyright pf)
+execute ExtraSourceFiles  pf = maybe (die "Missing Extra Source Files") (T.putStrLn . T.intercalate "\n") (packageExtraSourceFiles pf)
+execute Synopsis          pf = maybe (die "Missing Synopsis") T.putStrLn (packageSynopsis pf)
+execute Category          pf = maybe (die "Missing Category") T.putStrLn (packageCategory pf)
+execute Description       pf = maybe (die "Missing Description") T.putStrLn $ packageDescription pf
+execute Dependencies      pf = T.putStrLn $ T.intercalate ", " $ packageDependencies pf
