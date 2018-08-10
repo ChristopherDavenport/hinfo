59b34e4ddf14a2930aae12ffbd25ea58ecea8eac
Chris Davenport <chris@christopherdavenport.tech>
2018-08-10 14:53:31 -0400

    Update Version And Allow Optional Values Correctly

--- src/Hinfo.hs
+++ src/Hinfo.hs
@@ -7,6 +7,7 @@ module Hinfo
 import Options.Applicative                (execParser)
 import Hinfo.Hpack
 import Hinfo.Options
+import System.Exit
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
 
@@ -20,4 +21,4 @@ app = do
 execute :: AppOption -> PackageFile -> IO ()
 execute  Name        pf = T.putStrLn $ packageName pf
 execute  Version     pf = T.putStrLn $ packageVersion pf
-execute  Description pf = T.putStrLn $ packageDescription pf
+execute  Description pf = maybe (die "Missing Description") T.putStrLn $ packageDescription pf
--- src/Hinfo/Hpack/Internal/Types.hs
+++ src/Hinfo/Hpack/Internal/Types.hs
@@ -7,20 +7,20 @@ import qualified Data.Yaml    as Y
 import Data.Aeson             (FromJSON(..), Value, (.:), (.:?), withObject)
 
 data PackageFile = PackageFile {
-    packageName         :: T.Text
+    packageName                :: T.Text
   , packageVersion             :: T.Text
-  , packageGithub              :: T.Text
-  , packageLicense             :: T.Text
-  , packageAuthor              :: T.Text
-  , packageMaintainer          :: T.Text
-  , packageCopyright           :: T.Text
-  , packageExtraSourceFiles    :: [T.Text]
+  , packageGithub              :: Maybe T.Text
+  , packageLicense             :: Maybe T.Text
+  , packageAuthor              :: Maybe T.Text
+  , packageMaintainer          :: Maybe T.Text
+  , packageCopyright           :: Maybe T.Text
+  , packageExtraSourceFiles    :: Maybe [T.Text]
   , packageSynopsis            :: Maybe T.Text
   , packageCategory            :: Maybe T.Text
-  , packageDescription         :: T.Text
+  , packageDescription         :: Maybe T.Text
   , packageDependencies        :: [T.Text]
-  , packageExecutables         :: Value
-  , packageTests               :: Value
+  , packageExecutables         :: Maybe Value
+  , packageTests               :: Maybe Value
 } deriving (Show, Eq)
 
 
@@ -28,15 +28,15 @@ instance FromJSON PackageFile where
   parseJSON = withObject "PackageFile" $ \v -> PackageFile
     <$> v .: "name"
     <*> v .: "version"
-    <*> v .: "github"
-    <*> v .: "license"
-    <*> v .: "author"
-    <*> v .: "maintainer"
-    <*> v .: "copyright"
-    <*> v .: "extra-source-files"
+    <*> v .:? "github"
+    <*> v .:? "license"
+    <*> v .:? "author"
+    <*> v .:? "maintainer"
+    <*> v .:? "copyright"
+    <*> v .:? "extra-source-files"
     <*> v .:? "synopsis"
     <*> v .:? "category"
-    <*> v .: "description"
+    <*> v .:? "description"
     <*> v .: "dependencies"
-    <*> v .: "executables"
-    <*> v .: "tests"
+    <*> v .:? "executables"
+    <*> v .:? "tests"
